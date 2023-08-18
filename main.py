5a843d1dcc2e1a313bb36b5a594f2bf2d0945280
lake-crimsonn <lakecrimsonn@gmail.com>
2023-08-18 16:47:35 +0900

    file test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import warning_person
 video_path = 'full_vid.mp4'
 
 cap = cv2.VideoCapture(video_path)
-model = YOLO('before.pt')
+model = YOLO('best_yolo8n.pt')
 
 count = 0
 
@@ -18,16 +18,17 @@ while True:
     ret, frame = cap.read()
     yolo_frame = model(frame, verbose=False)
 
-    count += 1
-    if count % 60 == 0:
-        print(func_road.road(frame))
-    w_p_frame = warning_person.w_p(yolo_frame, deep_frame)
+    # 딥소트
+    deep_frame = deepsort_start(yolo_frame, frame)  # yolo, frame
 
     # 전방주시
     forward_looking(0, deep_frame)  # mode, frame
 
-    # 딥소트
-    deep_frame = deepsort_start(yolo_frame, frame)  # yolo, frame
+    # 로드워닝
+    count += 1
+    if count % 60 == 0:
+        print(func_road.road(frame))
+    w_p_frame = warning_person.w_p(yolo_frame, deep_frame)
 
     cv2.imshow('video', w_p_frame)
     if cv2.waitKey(1) == ord('q'):
