a0fa26e0faa3b71f157973d2a97166e0d10e6e5b
lake-crimsonn <lakecrimsonn@gmail.com>
2023-08-18 20:02:22 +0900

    모델, 비디오 디렉토리 생성

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ while True:
     # 딥소트
     deep_frame = deepsort_start(yolo_frame, frame)  # yolo, frame
 
-    # 전방주시
-    # forward_looking(0, deep_frame)  # mode, frame
-
     # 로드워닝
     count += 1
     if count % 60 == 0:
         print(func_road.road(frame))
     w_p_frame = warning_person.w_p(yolo_frame, deep_frame)
+
+    # 전방주시
+    # forward_looking(0, deep_frame)  # mode, frame
 
     cv2.imshow('video', w_p_frame)
     if cv2.waitKey(1) == ord('q'):
